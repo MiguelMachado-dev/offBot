ae53bf0fd5d1dc46a32f023b9cf77b873b9b81d4
Miguel Machado <miiguellmp@gmail.com>
2019-09-18 15:15:46 -0300

    comando: vagas, quarentena, chama, despacito

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ client.on("ready", () => {
   client.user.setPresence({
     status: "online",
     game: {
-      name: "a galera do off-topic não sendo contratado",
+      name: "o pessoal se drogando",
       type: "WATCHING"
     }
   });
 });
 
 client.on("message", async message => {
-  const prefix = "_"; // bot prefix
+  const prefix = "!"; // bot prefix
 
   if (message.author.bot) return;
   if (!message.guild) return;
@@ -44,11 +44,63 @@ client.on("message", async message => {
       .setAuthor("Regras do servidor:")
       .addField("É permitido?", "Sim")
       .addField("É proibido?", "Sim")
+      .addField("Demonstração de afeto?", "PROIBIDO")
       .setDescription("Pode zoar só não pode ofender.")
       .setFooter("Criado pelo offtopic Team")
       .setTimestamp();
 
     message.channel.send(embed);
+  }
+
+  if (cmd === "despacito") {
+    try {
+      await message.react("🇩");
+      await message.react("🇪");
+      await message.react("🇸");
+      await message.react("🇵");
+      await message.react("🇦");
+      await message.react("🇨");
+      await message.react("🇮");
+      await message.react("🇹");
+      await message.react("🇴");
+    } catch (error) {
+      console.error("One of the emojis failed to react.");
+    }
+  }
+
+  if (cmd === "chama") {
+    message.channel.send(`CHAMA NO MEU CARALIO, <@${message.author.id}>`);
+  }
+
+  if (cmd === "quarentena") {
+    // getting the member mentioned
+    const user = message.mentions.users.first();
+
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        message.channel.send(`O usuário ${user} foi enviado para quarentena!`);
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("Este usuário não está na guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("Você não mencionou um usuário.");
+    }
+  }
+
+  if (cmd === "vagas") {
+    message.channel.send(
+      `<@${message.author.id}> nunca conseguiria uma vaga ;)`
+    );
   }
 });
 
