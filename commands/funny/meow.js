aa7446659a8bd18a43ced4798dd8dcd1c07ea36e
Miguel Machado <miiguellmp@gmail.com>
2019-09-19 09:34:08 -0300

    comando meow

---
+++ commands/funny/meow.js
@@ -0,0 +1,28 @@
+const snek = require("snekfetch");
+const APIcat = "http://aws.random.cat/meow";
+
+module.exports = {
+  name: "meow",
+  category: "funny",
+  description: "Send a random dog media.",
+  run: async (client, message, args) => {
+    const msg = await message.channel.send("Tentando tirar foto do gato...");
+
+    const file = (await snek.get(APIcat)).body.file;
+    if (!file)
+      return message.channel.send(
+        "É, não deu... Foi rápido de mais! Tente de novo ;("
+      );
+
+    await message.channel.send({
+      files: [
+        {
+          attachment: file,
+          name: file.split("/").pop()
+        }
+      ]
+    });
+
+    msg.delete();
+  }
+};
