4defc52e593e96db72012bc8528416d5ae061a5a
Miguel Machado <miiguellmp@gmail.com>
2019-09-19 07:53:18 -0300

    sent a random dog media

---
+++ commands/funny/woof.js
@@ -0,0 +1,29 @@
+const snek = require("snekfetch");
+const APIdog = "https://random.dog/woof.json";
+module.exports = {
+  name: "woof",
+  category: "funny",
+  description: "Send a random dog media.",
+  run: async (client, message, args) => {
+    const msg = await message.channel.send(
+      "Tentando tirar foto do cachorro..."
+    );
+
+    const file = (await snek.get(APIdog)).body.url;
+    if (!file)
+      return message.channel.send(
+        "É, não deu... Foi rápido de mais! Tente de novo ;("
+      );
+
+    await message.channel.send({
+      files: [
+        {
+          attachment: file,
+          name: file.split("/").pop()
+        }
+      ]
+    });
+
+    msg.delete();
+  }
+};
