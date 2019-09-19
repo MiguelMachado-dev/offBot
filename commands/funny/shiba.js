019f27c76d403353165d50812bc599d61d28a140
Miguel Machado <miiguellmp@gmail.com>
2019-09-19 09:34:14 -0300

    comando shiba

---
+++ commands/funny/shiba.js
@@ -0,0 +1,29 @@
+const snek = require("snekfetch");
+const APIshiba =
+  "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true";
+
+module.exports = {
+  name: "shiba",
+  category: "funny",
+  description: "Send a random dog media.",
+  run: async (client, message, args) => {
+    const msg = await message.channel.send("Tentando tirar foto do shiba...");
+    const file = (await snek.get(APIshiba)).body[0];
+    console.log(file);
+    if (!file)
+      return message.channel.send(
+        "É, não deu... Foi rápido de mais! Tente de novo ;("
+      );
+
+    await message.channel.send({
+      files: [
+        {
+          attachment: file,
+          name: file.split("/").pop()
+        }
+      ]
+    });
+
+    msg.delete();
+  }
+};
