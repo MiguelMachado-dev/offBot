a22e3e7de3d383d037621760c06e18cfbed35132
Miguel Machado <miiguellmp@gmail.com>
2019-09-20 11:04:09 -0300

    weather command

---
+++ commands/info/weather.js
@@ -0,0 +1,38 @@
+const { RichEmbed } = require("discord.js");
+const axios = require("axios");
+const APIweather = "http://dataservice.accuweather.com/currentconditions/v1";
+const APItoGetKey =
+  "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+module.exports = {
+  name: "weather",
+  category: "info",
+  description: "Shows current location weather",
+  run: async (client, message, args) => {
+    const location = args.slice(0).join(" ");
+    const encodedLocation = encodeURI(location);
+    const urlParaDescobrirKey = `${APItoGetKey}?apikey=${process.env.WEATHER}&q=${encodedLocation}&language=pt-br`;
+    const keyResult = (await axios.get(urlParaDescobrirKey)).data;
+
+    const LocationKey = keyResult[0].Key;
+
+    const url = `${APIweather}/${LocationKey}?apikey=${process.env.WEATHER}&language=pt-br`;
+    const result = (await axios.get(url)).data;
+
+    const roleColor =
+      message.guild.me.displayHexColor === "#000000"
+        ? "#ffffff"
+        : message.guild.me.displayHexColor;
+
+    const embed = new RichEmbed()
+      .setColor(roleColor)
+      .setAuthor("Tempo:")
+      .setDescription(`${result[0].Link}`)
+      .addField("Tempo", `${result[0].WeatherText}`)
+      .addField("Temperatura", `${result[0].Temperature.Metric.Value}C`)
+      .setFooter("Criado pelo offtopic Team")
+      .setTimestamp();
+
+    message.channel.send(embed);
+  }
+};
