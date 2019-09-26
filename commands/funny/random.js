1bb2eb1364195eeb4b60d198fb66a3b0641d9d3e
Miguel Machado <miiguellmp@gmail.com>
2019-09-25 22:00:30 -0300

    random command

---
+++ commands/funny/random.js
@@ -0,0 +1,27 @@
+const snek = require("snekfetch");
+const APIdog = "https://meme-api.herokuapp.com/gimme";
+
+module.exports = {
+  name: "random",
+  category: "funny",
+  description: "Send a random meme.",
+  run: async (client, message, args) => {
+    const msg = await message.channel.send("> Calma ae");
+
+    const responseApi = (await snek.get(APIdog)).body;
+    const file = responseApi.url;
+
+    if (!file) return message.channel.send("É, não deu... Tente de novo ;(");
+
+    await message.channel.send(`${responseApi.title}`, {
+      files: [
+        {
+          attachment: file,
+          name: file.split("/").pop()
+        }
+      ]
+    });
+
+    msg.delete();
+  }
+};
