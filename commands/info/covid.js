119d78ae669da2f6d2294ebdc6e5351c4c6b84c5
Miguel Machado <miiguellmp@gmail.com>
2020-03-18 13:42:02 -0300

    fixing bot

--- commands/info/covid.js
+++ commands/info/covid.js
@@ -12,24 +12,27 @@ module.exports = {
 
     const ArrayDePaises = response.data.areas;
 
-    ArrayDePaises.map(pais => {
-      if (pais.id === "brazil") {
-        const embed = new RichEmbed()
-          .setColor("#FFA500")
-          .setDescription("Coronavirus Bing API")
-          .addField("Total confirmado:", `${response.data.totalConfirmed}`)
-          .addField(`${pais.displayName}:`, `${pais.totalConfirmed}`)
-          .addField("Atualizado em:", `${pais.lastUpdated}`)
-          .setFooter("Criado pelo offtopic Team")
-          .setTimestamp();
+    try {
+      ArrayDePaises.map(pais => {
+        if (pais.id === "brazil") {
+          const embed = new RichEmbed()
+            .setColor("#FFA500")
+            .setDescription("Coronavirus Bing API")
+            .addField("Total confirmado:", `${response.data.totalConfirmed}`)
+            .addField(`${pais.displayName}:`, `${pais.totalConfirmed}`)
+            .addField("Atualizado em:", `${pais.lastUpdated}`)
+            .setFooter("Criado pelo offtopic Team")
+            .setTimestamp();
 
-        message.channel.send(embed);
-      } else {
-        message.channel.send(
-          "Nao foi possivel localizar o Brasil na lista. Contate o adm"
-        );
-      }
-    });
+          message.channel.send(embed);
+        }
+      });
+    } catch (error) {
+      message.channel.send(error);
+      message.channel.send(
+        "Nao foi possivel localizar o Brasil na lista. Contate o adm"
+      );
+    }
 
     msg.delete();
   }
